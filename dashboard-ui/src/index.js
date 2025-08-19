a210100833272b64840b185e7cd42e11981f7533
akckwok <57162122+akckwok@users.noreply.github.com>
2025-08-19 01:38:15 -0400

    Revert "feat: Add user authentication feature"
    This reverts commit 454ac7c8738c82ec277274b4d36f288fc8b6895c.

--- dashboard-ui/src/index.js
+++ dashboard-ui/src/index.js
@@ -1,8 +1,9 @@
-import './App.js';
+import { initializeRouter } from './App.js';
 import { initializeJobSubmissionPage } from './pages/JobSubmission.js';
 import { initializeAllJobsPage } from './pages/AllJobs.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    initializeRouter();
     initializeJobSubmissionPage();
     initializeAllJobsPage();
 });
